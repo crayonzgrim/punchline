83b848e32c7686c68340ef0754eca0c5909251a1
KIM DONG IL <crayonzgrim@gmail.com>
2025-06-22 22:19:51 +0900

    🔨 User module, entity

---
+++ backend/src/user/user.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true })
+  nickname: string;
+
+  @Column({ default: 'user' })
+  role: 'user' | 'admin';
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
