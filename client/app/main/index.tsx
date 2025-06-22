c111869c409b4b4ed1feabbe684f91e22d2d59ea
KIM DONG IL <crayonzgrim@gmail.com>
2025-06-22 14:06:22 +0900

    🔨 Main Tabs

--- client/app/main/index.tsx
+++ client/app/main/index.tsx
@@ -1,7 +1,7 @@
+import { COLORS } from "@/constant/global-colors";
 import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
 import BoardScreen from "./BoardScreen";
 import HomeScreen from "./HomeScreen";
 import SettingsScreen from "./SettingsScreen";
@@ -10,47 +10,47 @@ const Tab = createBottomTabNavigator();
 
 export default function MainTabs() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-            if (route.name === "HomeTab") {
-              iconName = focused ? "home" : "home-outline";
-            } else if (route.name === "BoardTab") {
-              iconName = focused ? "list" : "list-outline";
-            } else if (route.name === "SettingsTab") {
-              iconName = focused ? "settings" : "settings-outline";
-            }
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Board") {
+            iconName = focused ? "list" : "list-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "settings-outline";
+          }
 
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: "#e53935",
-          tabBarInactiveTintColor: "#888",
-          tabBarStyle: {
-            paddingBottom: 5,
-            paddingTop: 5,
-          },
-        })}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ tabBarLabel: "홈" }}
-        />
-        <Tab.Screen
-          name="Board"
-          component={BoardScreen}
-          options={{ tabBarLabel: "게시판" }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={SettingsScreen}
-          options={{ tabBarLabel: "설정" }}
-        />
-      </Tab.Navigator>
-    </SafeAreaView>
+          return (
+            <Ionicons name={iconName as any} size={size - 5} color={color} />
+          );
+        },
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: COLORS.background,
+        tabBarStyle: {
+          paddingHorizontal: 5,
+          paddingVertical: 5,
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ tabBarLabel: "홈" }}
+      />
+      <Tab.Screen
+        name="Board"
+        component={BoardScreen}
+        options={{ tabBarLabel: "게시판" }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{ tabBarLabel: "설정" }}
+      />
+    </Tab.Navigator>
   );
 }
