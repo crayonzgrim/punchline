e6a3d0890b9a4e54df575c066f429b22236f308c
KIM DONG IL <dongil.kim@anti-agingclub.kr>
2025-07-08 10:19:25 +0900

    :hammer: Temp module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { BoardPost } from './board-post/board-post.entity';
+import { PostModule } from './board-post/board-post.module';
+import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -11,16 +14,21 @@ import { UserModule } from './user/user.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
+      host: 'localhost',
       port: 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity.{ts,js}'],
+      // username: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD,
+      // database: process.env.DB_NAME,
+      // entities: [__dirname + '/**/*.entity.{ts,js}'],
+      username: 'root',
+      password: 'abcd1234*',
+      database: 'punchline',
+      entities: [User, BoardPost],
       synchronize: true,
     }),
     UserModule,
     AuthModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
